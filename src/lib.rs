edd1f626614ba4aed9e3a0e2ef1d8653f1967f0b
turtle-sandwich <turtlesandwich@pm.me>
2024-12-03 11:39:57 +0100

    day 3 part 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 pub mod day01;
 pub mod day02;
+pub mod day03;
 
 /// Common return type of puzzle solutions
 pub type PuzzleResult = Result<String, Box<dyn std::error::Error>>;
@@ -10,6 +11,7 @@ pub fn solve(day: usize, part: usize, input: String) -> PuzzleResult {
         (1, 2) => day01::part2(input),
         (2, 1) => day02::part1(input),
         (2, 2) => day02::part2(input),
+        (3, 1) => day03::part1(input),
         (day, part) => Err(format!("no solution for day {day} part {part}"))?,
     }
 }
