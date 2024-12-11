d8fe4e9ac6ae173e916a695ad0029bf5780c9892
turtle-sandwich <turtlesandwich@pm.me>
2024-12-11 16:25:49 +0100

    day 10 part 2

--- src/lib.rs
+++ src/lib.rs
@@ -33,6 +33,7 @@ pub fn solve(day: usize, part: usize, input: String) -> PuzzleResult {
         (9, 1) => day09::part1(input),
         (9, 2) => day09::part2(input),
         (10, 1) => day10::part1(input),
+        (10, 2) => day10::part2(input),
         (day, part) => Err(format!("no solution for day {day} part {part}"))?,
     }
 }
