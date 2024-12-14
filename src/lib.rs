8c844a683629013df332434e0856133d783611e8
turtle-sandwich <turtlesandwich@pm.me>
2024-12-15 00:09:36 +0100

    day 12 part 2

--- src/lib.rs
+++ src/lib.rs
@@ -41,6 +41,7 @@ pub fn solve(day: usize, part: usize, input: String) -> PuzzleResult {
         (11, 1) => day11::part1(input),
         (11, 2) => day11::part2(input),
         (12, 1) => day12::part1(input),
+        (12, 2) => day12::part2(input),
         (13, 1) => day13::part1(input),
         (13, 2) => day13::part2(input),
         (14, 1) => day14::part1(input),
